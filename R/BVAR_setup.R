c9375570d203af0ea41c208ce73764ea647cd0f6
Mark Becker <mark.jw.becker@gmail.com>
2025-10-19 09:26:50 +0200

    some changes

--- R/BVAR_setup.R
+++ R/BVAR_setup.R
@@ -1,4 +1,4 @@
-BVAR_setup <- function(Z, p, det=c("c", "c&t", "c&d"), dummy=NULL) {
+BVAR_setup <- function(Z, p, det=c("c", "c&d"), dummy=NULL) {
 
   N = dim(Z)[1]-p
   m = dim(Z)[2]
@@ -6,20 +6,19 @@ BVAR_setup <- function(Z, p, det=c("c", "c&t", "c&d"), dummy=NULL) {
   det <- match.arg(det)
   if (det == "c") {
     x <- cbind(rep(1, nrow(Z)))
-  } else if (det == "c&t") {
-    x <- cbind(rep(1, nrow(Z)), 1:nrow(Z))
-  } else { # det == "c&d"
+  } else {
     if (is.null(dummy)) {
-      print("input dummy variable")
+      print("please input dummy")
     } else {
       x <- cbind(rep(1, nrow(Z)), dummy)
     }
   }
-
+  
   Y <- Z[-c(1:p), ]
   W <- embed(Z, dimension = p+1)[, -(1:m)]
   X <- x[-c(1:p), ]
   Q <- embed(x, dimension = p+1)[, -(1:d)]
-  obj=list(N=N,m=m,p=p,Y=Y,X=X,W=W,Q=Q)
-  return(obj)
+  
+  stan_data=list(N=N,m=m,p=p,Y=Y,X=X,W=W,Q=Q)
+  return(stan_data)
 }
