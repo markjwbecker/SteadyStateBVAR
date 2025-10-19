8a2c4d65aec0b0c87e2f8831762dd6f150160608
Mark Becker <mark.jw.becker@gmail.com>
2025-10-19 11:39:00 +0200

    asdasd

--- R/BVAR_setup.R
+++ R/BVAR_setup.R
@@ -2,16 +2,16 @@ BVAR_setup <- function(Z, p, det=c("c", "c&d"), dummy=NULL) {
 
   N = dim(Z)[1]-p
   m = dim(Z)[2]
-  d=2
   det <- match.arg(det)
   if (det == "c") {
     x <- cbind(rep(1, nrow(Z)))
-  } else {
-    if (is.null(dummy)) {
-      print("please input dummy")
-    } else {
-      x <- cbind(rep(1, nrow(Z)), dummy)
-    }
+    d <- 1
+  } else if (det == "c&t") {
+    x <- cbind(rep(1, nrow(Z)), 1:nrow(Z))
+    d <- 2
+  } else if (det == "c&d"){
+    x <- cbind(rep(1, nrow(Z)), dummy)
+    d <- 2
   }
   
   Y <- Z[-c(1:p), ]
@@ -19,6 +19,6 @@ BVAR_setup <- function(Z, p, det=c("c", "c&d"), dummy=NULL) {
   X <- x[-c(1:p), ]
   Q <- embed(x, dimension = p+1)[, -(1:d)]
   
-  stan_data=list(N=N,m=m,p=p,Y=Y,X=X,W=W,Q=Q)
+  stan_data=list(N=N,m=m,p=p,Y=Y,X=X,W=W,Q=Q,d=d)
   return(stan_data)
 }
