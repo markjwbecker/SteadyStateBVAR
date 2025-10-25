a2d784943ad0e5b50c142e56f04c6420e6d9fe04
Mark Becker <mark.jw.becker@gmail.com>
2025-10-25 17:48:26 +0200

    dsad

--- R/plot.R
+++ R/plot.R
@@ -11,14 +11,14 @@ plot_forecast <- function(fit, Y, ci=0.95, fcst_type=c("mean", "median"), growth
   m <- ncol(Y)
   
   time_hist <- time(Y)
-  time_fore <- seq(tail(time_hist, 1) + 1/4, by = 1/4, length.out = H)
+  time_fore <- seq(tail(time_hist, 1) + 0.25, by = 0.25, length.out = H)
   
   if (ncol(Y) < 4){
-    par(mfrow = c(ncol(Y), 1))
+    par(mfcol = c(ncol(Y), 1))
   } else if ((ncol(Y) < 9)){
-    par(mfrow = c(4,2))
+    par(mfcol = c(4,2))
   } else {
-    par(mfrow = c(1,1))
+    par(mfcol = c(1,1))
   }
   on.exit(par(mfrow = c(1,1)))
   for (i in 1:ncol(Y)) {
@@ -34,16 +34,21 @@ plot_forecast <- function(fit, Y, ci=0.95, fcst_type=c("mean", "median"), growth
         annual_hist[t] <- sum(smply[(t-3):t])
       }
       annual_hist <- annual_hist / 4
+      annual_hist <- ts(annual_hist, start = start(Y), frequency = 4)
+      
       last_obs <- tail(smply,3)
-      all_quarters <- c(last_obs, fcst_m)
+      all_fcst <- c(last_obs, fcst_m)
+      
       annual_fcst <- rep(NA, H)
       for(t_h in 1:H){
-        annual_fcst[t_h] <- sum(all_quarters[t_h:(t_h+3)])
+        annual_fcst[t_h] <- sum(all_fcst[t_h:(t_h+3)])
       }
       annual_fcst <- annual_fcst / 4
       
+      
       all_lower <- c(last_obs, fcst_lower)
       all_upper <- c(last_obs, fcst_upper)
+      
       annual_lower <- rep(NA,H)
       annual_upper <- rep(NA,H)
       for(t_h in 1:H){
@@ -52,30 +57,27 @@ plot_forecast <- function(fit, Y, ci=0.95, fcst_type=c("mean", "median"), growth
       }
       annual_lower <- annual_lower / 4
       annual_upper <- annual_upper / 4
-      time_full_hist <- time_hist
-      m_full_hist <- annual_hist
-      time_full <- c(time_full_hist, time_fore)
-      m_full <- c(m_full_hist, annual_fcst)
-      lower_full <- c(m_full_hist, annual_lower)
-      upper_full <- c(m_full_hist, annual_upper)
       
-      ylim <- range(c(m_full_hist, lower_full, upper_full), na.rm=TRUE)
+      time_full <- c(tail(time_hist, 1), time_fore)
       
-      plot(time_full_hist, m_full_hist, type="l", col="black", lwd=2,
-           xlim=c(time_full[1], tail(time_full,1)),
-           ylim=ylim,
-           ylab="",
-           xlab="Time",
-           main=paste(colnames(Y)[i], "(annual)"))
+      m_full <- c(tail(annual_hist, 1), annual_fcst)
+      lower_full <- c(tail(annual_hist, 1), annual_lower)
+      upper_full <- c(tail(annual_hist, 1), annual_upper)
       
-      polygon(c(time_full, rev(time_full)),
-              c(upper_full, rev(lower_full)),
-              col=rgb(0,0,1,0.2), border=NA)
+      ylim <- range(c(annual_hist, annual_lower, annual_upper),na.rm=TRUE)
       
-      fcst_line <- c(tail(annual_hist, 1), annual_fcst)
-      time_fcst <- seq(tail(time_hist, 1), by = 1/4, length.out = H + 1)
-      lines(time_fcst, fcst_line, col="blue", lwd=2)
+      plot.ts(annual_hist, main = paste(colnames(Y)[i], "(annual)"), xlab = "Time", ylab = NULL,
+              xlim = c(time_hist[1], tail(time_fore, 1)),
+              ylim = ylim,col = "black", lwd = 2)
       
+      polygon(
+        c(time_full, rev(time_full)),
+        c(upper_full, rev(lower_full)),
+        col = rgb(0, 0, 1, 0.2),
+        border = NA
+      )
+      
+      lines(time_full, m_full, col = "blue", lwd = 2)
       
     } else {
       
@@ -88,7 +90,7 @@ plot_forecast <- function(fit, Y, ci=0.95, fcst_type=c("mean", "median"), growth
       
       plot.ts(smply, main = colnames(Y)[i], xlab = "Time", ylab = NULL,
               xlim = c(time_hist[1], tail(time_fore, 1)),
-              ylim = ylim, col = "black", lwd = 2)
+              ylim = ylim,col = "black", lwd = 2)
       
       polygon(
         c(time_full, rev(time_full)),
@@ -97,7 +99,6 @@ plot_forecast <- function(fit, Y, ci=0.95, fcst_type=c("mean", "median"), growth
         border = NA
       )
       lines(time_full, m_full, col = "blue", lwd = 2)
-      
     }
   }
 }
