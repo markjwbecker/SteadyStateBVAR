aea2680e15210cfb14bb8038948dc7456b671593
Mark Becker <mark.jw.becker@gmail.com>
2025-10-29 00:28:01 +0100

    sdksoadkoas

--- R/BVAR_setup.R
+++ R/BVAR_setup.R
@@ -1,24 +1,21 @@
-BVAR_setup <- function(Z, p, det=c("c", "c&d"), dummy=NULL) {
-
-  N = dim(Z)[1]-p
-  m = dim(Z)[2]
-  det <- match.arg(det)
-  if (det == "c") {
-    x <- cbind(rep(1, nrow(Z)))
-    d <- 1
-  } else if (det == "c&t") {
-    x <- cbind(rep(1, nrow(Z)), 1:nrow(Z))
-    d <- 2
-  } else if (det == "c&d"){
-    x <- cbind(rep(1, nrow(Z)), dummy)
-    d <- 2
+BVAR_setup <- function(yt, p, deterministic=c("constant", "constant_and_dummy"), dummy=NULL) {
+  
+  N = dim(yt)[1]-p
+  k = dim(yt)[2]
+  deterministic <- match.arg(deterministic)
+  if (deterministic == "constant") {
+    x <- cbind(rep(1, nrow(yt)))
+    q <- 1
+  } else {
+    x <- cbind(rep(1, nrow(yt)), dummy)
+    q <- 2
   }
   
-  Y <- Z[-c(1:p), ]
-  W <- embed(Z, dimension = p+1)[, -(1:m)]
+  Y <- yt[-c(1:p), ]
+  W <- embed(yt, dimension = p+1)[, -(1:k)]
   X <- x[-c(1:p), ]
-  Q <- embed(x, dimension = p+1)[, -(1:d)]
+  Q <- embed(x, dimension = p+1)[, -(1:q)]
   
-  stan_data=list(N=N,m=m,p=p,Y=Y,X=X,W=W,Q=Q,d=d)
+  stan_data=list(N=N,k=k,p=p,Y=Y,X=X,W=W,Q=Q,q=q)
   return(stan_data)
 }
