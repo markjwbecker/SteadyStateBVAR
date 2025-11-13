4a18b8af429f936a338018a3b20cdffc9c90a138
Mark Becker <mark.jw.becker@gmail.com>
2025-11-13 19:57:13 +0100

    changed ppi

--- R/ppi.R
+++ R/ppi.R
@@ -2,7 +2,7 @@ ppi <- function(l, u, alpha = 0.05, annualized_growthrate = FALSE, freq=4) {
 
   z <- qnorm(1 - alpha/2)
   
-  if (!annualize_growthrate) {
+  if (!annualized_growthrate) {
     sigma <- (u - l) / (2 * z)
     mu <- (u + l) / 2
   } else {
