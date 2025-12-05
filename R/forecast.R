922cb095de78b3023dd5192eb0c6e57ff871ec34
Mark Becker <mark.jw.becker@gmail.com>
2025-12-05 10:30:14 +0100

    changeforecast

--- R/forecast.R
+++ R/forecast.R
@@ -1,18 +1,11 @@
 forecast <- function(x,
                      ci=0.95,
-                     sd=FALSE,
                      fcst_type=c("mean", "median"), 
-                     growth_rate_idx=NULL, 
-                     plot_idx=NULL, 
+                     growth_rate_idx=NULL,
+                     plot_idx=NULL,
                      estimation = c("stan", "gibbs"),
-                     xlim=NULL,
-                     ylim=NULL)
-  {
-    
-  
-  
-  xlim_user <- xlim
-  ylim_user <- ylim
+                     show_all=FALSE)
+{
   Y <- x$data
   freq <- frequency(Y)
   if (is.null(plot_idx)) plot_idx <- 1:ncol(Y)
@@ -20,30 +13,18 @@ forecast <- function(x,
   if (estimation=="gibbs") {
     Y_pred <- x$fit$gibbs$fcst_draws
     Y_pred_m <- apply(Y_pred, c(1, 2), fcst_type)
-    if (sd==FALSE){
-      alpha <- 1 - ci
-      Y_pred_lower <- apply(Y_pred, c(1, 2), quantile, probs = alpha/2)
-      Y_pred_upper <- apply(Y_pred, c(1, 2), quantile, probs = 1 - alpha/2)
-    } else {
-      Y_pred_sd <- apply(Y_pred, c(1, 2), sd)
-      Y_pred_lower <- Y_pred_m - Y_pred_sd
-      Y_pred_upper <- Y_pred_m + Y_pred_sd
-    }
-
+    alpha <- 1 - ci
+    Y_pred_lower <- apply(Y_pred, c(1, 2), quantile, probs = alpha/2)
+    Y_pred_upper <- apply(Y_pred, c(1, 2), quantile, probs = 1 - alpha/2)
   } else {
     posterior <- rstan::extract(x$fit$stan)
     Y_pred <- posterior$Y_pred
     Y_pred_m <- apply(Y_pred, c(2, 3), fcst_type)
-    if (sd==FALSE){
     alpha <- 1 - ci
     Y_pred_lower <- apply(Y_pred, c(2, 3), quantile, probs = alpha/2)
     Y_pred_upper <- apply(Y_pred, c(2, 3), quantile, probs = 1 - alpha/2)
-    } else {
-      Y_pred_sd <- apply(Y_pred, c(2, 3), sd)
-      Y_pred_lower <- Y_pred_m - Y_pred_sd
-      Y_pred_upper <- Y_pred_m + Y_pred_sd
-    }
   }
+  
   
   T <- nrow(Y)
   H <- nrow(Y_pred_m)
@@ -99,24 +80,23 @@ forecast <- function(x,
       ylim <- range(c(annual_lower, annual_upper),na.rm=TRUE)
       ymin <- floor(ylim[1]*2)/2
       ymax <- ceiling(ylim[2]*2)/2
-      yticks <- seq(ymin-100, ymax+100, by = 0.5)
+      yticks <- seq(ymin, ymax, by = 0.5)
       
-      if (!is.null(xlim) && !is.null(ylim)) {
+      if (isFALSE(show_all)){
         plot.ts(annual_hist, main = paste(colnames(Y)[i], "(annual)"), xlab = "Time", ylab = NULL,
-                col = "black", lwd = 2,xlim=xlim_user,
-                ylim= ylim_user, yaxt="n")
-        abline(h = seq(ymin-100, ymax+100, by = 0.5), col = "gray", lty = 2)
+                xlim = c(head(time_fore,1)-(freq*2),tail(time_fore,1)),
+                ylim = ylim,col = "black", lwd = 2, yaxt = "n")
+        xlim_vals <- c(head(time_hist, 1), tail(time_fore, 1))
+        x_quarters <- seq(xlim_vals[1], xlim_vals[2], by = 1/freq)
+        abline(v = x_quarters, col = "gray", lty = 2) 
+        abline(h = seq(ymin, ymax, by = 0.5), col = "gray", lty = 2)
         axis(side = 2, at = yticks, labels = yticks, las = 1)
         points(as.numeric(time_hist), annual_hist, pch=16, col="black")
         points(time_full[-1], m_full[-1], pch=16, col="blue")
       } else {
         plot.ts(annual_hist, main = paste(colnames(Y)[i], "(annual)"), xlab = "Time", ylab = NULL,
-                col = "black", lwd = 2,xlim=c(tail(time_full,1)-10, tail(time_fore, 1)),
-                ylim=range(upper_full,lower_full,c(m_full), na.rm=TRUE), yaxt="n")
-        abline(h = seq(ymin-100, ymax+100, by = 0.5), col = "gray", lty = 2)
-        axis(side = 2, at = yticks, labels = yticks, las = 1)
-        points(as.numeric(time_hist), annual_hist, pch=16, col="black")
-        points(time_full[-1], m_full[-1], pch=16, col="blue")
+                col = "black", lwd = 2,xlim=c(head(time_hist, 1), tail(time_fore, 1)),
+                ylim=range(upper_full,lower_full,c(annual_hist), na.rm=TRUE))
       }
       
       polygon(
@@ -137,26 +117,23 @@ forecast <- function(x,
       ylim <- range(c(lower_full, upper_full))
       ymin <- floor(ylim[1]*2)/2
       ymax <- ceiling(ylim[2]*2)/2
-      yticks <- seq(ymin-100, ymax+100, by = 0.5)
+      yticks <- seq(ymin, ymax, by = 0.5)
       
-      if (!is.null(xlim) && !is.null(ylim)) {
-      plot.ts(smply, main = colnames(Y)[i], xlab = "Time", ylab = NULL,
-              col = "black", lwd = 2,xlim=xlim_user,
-              ylim= ylim_user, yaxt="n")
-        
-      abline(h = seq(ymin-100, ymax+100, by = 0.5), col = "gray", lty = 2)
-      axis(side = 2, at = yticks, labels = yticks, las = 1)
-      points(as.numeric(time_hist), smply, pch=16, col="black")
-      points(time_full[-1], m_full[-1], pch=16, col="blue")
-      
-     } else {
+      if (isFALSE(show_all)){
         plot.ts(smply, main = colnames(Y)[i], xlab = "Time", ylab = NULL,
-                col = "black", lwd = 2,xlim=c(tail(time_full, 1)-10, tail(time_fore, 1)),
-                ylim=range(upper_full,lower_full,m_full), yaxt="n")
-       abline(h = seq(ymin-100, ymax+100, by = 0.5), col = "gray", lty = 2)
-       axis(side = 2, at = yticks, labels = yticks, las = 1)
-       points(as.numeric(time_hist), smply, pch=16, col="black")
-       points(time_full[-1], m_full[-1], pch=16, col="blue")
+                xlim = c(head(time_fore,1)-(freq*2),tail(time_fore,1)),
+                ylim = ylim,col = "black", lwd = 2, yaxt = "n")
+        xlim_vals <- c(head(time_hist, 1), tail(time_fore, 1))
+        x_quarters <- seq(xlim_vals[1], xlim_vals[2], by = 1/freq)
+        abline(v = x_quarters, col = "gray", lty = 2) 
+        abline(h = seq(ymin, ymax, by = 0.5), col = "gray", lty = 2)
+        axis(side = 2, at = yticks, labels = yticks, las = 1)
+        points(as.numeric(time_hist), smply, pch=16, col="black")
+        points(time_full[-1], m_full[-1], pch=16, col="blue")
+      } else {
+        plot.ts(smply, main = colnames(Y)[i], xlab = "Time", ylab = NULL,
+                col = "black", lwd = 2,xlim=c(head(time_hist, 1), tail(time_fore, 1)),
+                ylim=range(upper_full,lower_full,smply))
       }
       
       polygon(
@@ -171,5 +148,6 @@ forecast <- function(x,
   x$forecasts$forecast = Y_pred_m
   x$forecasts$lower = Y_pred_lower
   x$forecasts$upper = Y_pred_upper
+  
   return(x)
 }
